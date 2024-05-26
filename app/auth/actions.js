c3b6c6a04b52165f8d27ab2351fd3f94929d88d2
braile <braile.c.gawigawen@gmail.com>
2024-05-26 14:23:27 +0800

    remove the trailing / in the getURL()

--- app/auth/actions.js
+++ app/auth/actions.js
@@ -12,7 +12,7 @@ const getURL = () => {
   // Make sure to include `https://` when not localhost.
   url = url.includes("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
-  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+  // url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
   return url;
 };
 
