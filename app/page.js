a73c4cb201abf5243545aba39c379ec08a9013b0
braile <braile.c.gawigawen@gmail.com>
2024-06-01 10:38:58 +0800

    [UNDER MAINTENANCE] working on fixes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 "use server";
 import { redirect } from "next/navigation";
 export default async function Home() {
-  redirect("/auth");
+  return (
+    <div className="flex items-center justify-center min-h-dvh">
+      Under Maintenance. Working on Fixes.
+    </div>
+  );
+  // redirect("/auth");
 }
